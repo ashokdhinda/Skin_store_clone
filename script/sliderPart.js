abf7a8500cfd5b0be4678367f59c8003a15afe07
ashokdhinda <ashokkudhinda143@gmail.com>
2024-09-14 13:00:02 +0530

    slider part all completd

--- script/sliderPart.js
+++ script/sliderPart.js
@@ -7,16 +7,16 @@ let arr = [
   `/Skin_store_clone/images/original-Page-007-035233.webp`,
 ];
 let currentIndex = 0;
-
+function ubdateImage() {
+    main_img.src=arr[currentIndex]
+}
 prev_img.addEventListener('click',()=>{
     if (currentIndex == 0) {
         currentIndex = arr.length - 1
     } else {
         currentIndex--
     }
-    main_img.src = arr[currentIndex]
-    console.log(main_img);
-    
+    ubdateImage()
 })
 
 next_img.addEventListener('click', () => {
@@ -25,7 +25,7 @@ next_img.addEventListener('click', () => {
     } else {
         currentIndex++
     }
-    main_img.src = arr[currentIndex]  
+    ubdateImage()
 })
 setInterval(() => {
     if (currentIndex === arr.length-1) {
@@ -33,5 +33,6 @@ setInterval(() => {
     } else {
         currentIndex++
     }
-    main_img.src = arr[currentIndex]
-},2000)
+    ubdateImage()
+}, 2000)
+
