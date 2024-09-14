e19528eb06ca9172fd447f9fdb1f77c07bea9c8a
ashokdhinda <ashokkudhinda143@gmail.com>
2024-09-14 16:45:15 +0530

    signup page complete

---
+++ script/signUp.js
@@ -0,0 +1,18 @@
+let form = document.getElementById("form")
+let name = document.getElementById("name");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+
+form.addEventListener("submit", 
+    function (event) {
+        event.preventDefault();
+        let userDetails = {
+            name: name.value,
+            email: email.value,
+            password:password.value
+        }
+        localStorage.setItem("userDetails", JSON.stringify(userDetails));
+        console.log(userDetails);
+        window.location.href = "/Skin_store_clone/html/login.html"
+    }
+)
