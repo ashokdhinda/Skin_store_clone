05a29c40fc92087687ab830e5c1e15cc6b90f10b
ashokdhinda <ashokkudhinda143@gmail.com>
2024-09-14 17:27:33 +0530

    login page \ page complete

---
+++ script/login_.js
@@ -0,0 +1,20 @@
+let form = document.getElementById("form")
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    let useDetails = {
+        email: email.value,
+        password: password.value,
+    }
+    let saveItem = JSON.parse(localStorage.getItem("userDetails")) || {};
+    if (
+        saveItem.email == useDetails.email && saveItem.password == useDetails.password
+    ) {
+        alert("Login Success")
+        window.location.href = "/Skin_store_clone/html/Main/myAccount.html"
+    } else {
+        alert("worng crendetial")
+    }
+})
