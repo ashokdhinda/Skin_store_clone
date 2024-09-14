a17d2911aedce2a04c1803e92e82c902e6c386fb
ashokdhinda <ashokkudhinda143@gmail.com>
2024-09-14 19:00:54 +0530

    item product card complete

---
+++ script/itemProduct.js
@@ -0,0 +1,23 @@
+const increase = document.getElementById("increase");
+const decrease = document.getElementById("decrease");
+const count = document.getElementById("num-de-in");
+const addButton = document.getElementById("button-part");
+
+increase.addEventListener("click", () => {
+    let num = Number(count.textContent)
+    num+=1
+    count.textContent = num
+    
+    
+})
+decrease.addEventListener("click", ()=>{
+    let num = Number(count.textContent)
+    num -= 1
+    if (num >= 0) {
+        count.textContent = num;
+    }
+    
+})
+addButton.addEventListener("click", () => {
+    alert("item is add successfully");
+})
