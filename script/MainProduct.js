036df5c7a438bd8f64a8e6bdf0e885751d058a18
ashokdhinda <ashokkudhinda143@gmail.com>
2024-09-15 12:52:25 +0530

    project full completed

--- script/MainProduct.js
+++ script/MainProduct.js
@@ -1,69 +1,70 @@
+
 document.addEventListener("DOMContentLoaded", async function () {
-    let products;
-    try {
-        let res = await fetch ("/Skin_store_clone/json/product.json")
-        let data = await res.json()
-        products = data
-        // console.log(products, "eefc");
-        disPlayData(products)
-    } catch (error) {
-        console.log(error); 
-    }
-        let lowToHighBtn = document.getElementById("lowToHigh");    
-        let highTolowBTn = document.getElementById("highToLow")
+  let products;
+  try {
+    let res = await fetch("/Skin_store_clone/json/product_one.json");
+    let data = await res.json();
+    products = data;
+    console.log(products);
+    displayData(products); 
+  } catch (error) {
+    console.log(error);
+  }
 
-    lowToHighBtn.addEventListener('click', () => {
-        products.sort((a, b) => parseFloat(a.price) - parseFloat(b.price))
-        // console.log(products);
-        disPlayData(products)
-    })
-           
-           
-    highTolowBTn.addEventListener("click", () => {
-        products.sort((a,b)=> parseFloat(b.price) - parseFloat(a.price))
-        // console.log(products);
-        disPlayData(products)
-        
-    })
-})
+  let lowToHighBtn = document.getElementById("lowToHigh");
+  let highToLowBtn = document.getElementById("highToLow"); 
 
-function disPlayData(products) {
-    const productionSection = document.getElementById("product-section");
-    productionSection.innerHTML = ""
+  lowToHighBtn.addEventListener("click", () => {
+    products.sort((a, b) => parseFloat(a.price) - parseFloat(b.price));
+    console.log(products);
+    displayData(products);
+  });
 
-    products.forEach(product => {
-        const productCard = createProductCard(product)
-        productionSection.appendChild(productCard)
-    })
-}
+  
+  highToLowBtn.addEventListener("click", () => {
+    products.sort((a, b) => parseFloat(b.price) - parseFloat(a.price));
+    console.log(products);
+    displayData(products);
+  });
+});
 
-function createProductCard(product) {
-    const productDiv = document.createElement("div")
-    productDiv.className = "product-div"
+function displayData(products) {
+  const productionSection = document.getElementById("product-section");
+    productionSection.innerHTML = "";
 
-    const img = document.createElement("img")
-    img.src = product.image;
-    img.alt = product.name;
-    productDiv.appendChild(img)
+  products.forEach(function (ele) {
+    let cardDiv = document.createElement("div");
+    cardDiv.className = "product-card";
 
-    const name = document.createElement("p")
-    name.textContent = product.name;
-    productDiv.appendChild(name)
+    const img = document.createElement("img");
+    img.src = ele.image;
+    img.alt = ele.name;
+    cardDiv.appendChild(img);
 
-    const discount = document.createElement("p")
-    discount.innerHTML = `${product.discount} <span>SKINM!)</span>`
-    productDiv.appendChild(discount)
+    const name = document.createElement("p");
+    name.textContent = ele.name;
+    cardDiv.appendChild(name);
 
-    const price = document.createElement("p")
-    price.textContent = `${product.price}`
-    productDiv.appendChild(price)
+    const discount = document.createElement("p");
+    discount.innerHTML = `${ele.discount} <span>SKINM10</span>`;
+    cardDiv.appendChild(discount);
 
-    const quickBuy = document.createElement("button")
-    quickBuy.textContent = "BUY NOW"
-    quickBuy.addEventListener('click', function () {
-        window.location.href = "/Skin_store_clone/html/itemProduct.html"
-    })
-    productDiv.appendChild(quickBuy)
-    return productDiv
+    const price = document.createElement("p");
+    price.textContent = `$ ${ele.price}`;
+    cardDiv.appendChild(price);
 
+    const quickBuyBtn = document.createElement("button");
+    quickBuyBtn.textContent = "QUICK BUY";
+    quickBuyBtn.addEventListener("click", function () {
+      alert("Product added to cart");
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+      cart.push(ele);
+      localStorage.setItem("cart", JSON.stringify(cart));
+
+      window.location.href = `/Skin_store_clone/html/addTocart.html`;
+    });
+    cardDiv.appendChild(quickBuyBtn);
+
+    productionSection.appendChild(cardDiv);
+  });
 }
